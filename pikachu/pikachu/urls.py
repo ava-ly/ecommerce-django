0fad23cdd05cd3a501908df31c5c403e410235c8
CodeSandbox <support@codesandbox.io>
2024-09-28 14:59:55 +0000

    Sandbox snapshot commit

--- pikachu/pikachu/urls.py
+++ pikachu/pikachu/urls.py
@@ -22,8 +22,7 @@ from django.urls import path, include
 from core.views import index, contact
 
 urlpatterns = [
-    path('', index, name='index'),
-    path('items/', include('items.urls')),
-    path('contact/', contact, name='contact'),
+    path('', include('core.urls')),
+    path('item/', include('item.urls')),
     path("admin/", admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
